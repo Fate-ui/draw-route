dc131629ea967aaf0f84ef344cdafbd41dace7ec
ween <228574227@qq.com>
2024-03-06 18:00:31 +0800

    删除冗余代码

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import { createVitePlugins } from './config/vitePlugin'
-import { buildConfig } from './config/build'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -14,20 +13,18 @@ export default defineConfig(({ mode }) => {
       }
     },
     plugins: createVitePlugins(),
-    esbuild: {
-      pure: ['console.log'] // 去除console.log
-    },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          charset: false,
-          //引入variables.module.scss、mixin.scss，在vue组件和其他scss文件中可直接使用，无需再次引用，再次引用会报错
-          additionalData: `
-          @use "@/style/variables.module.scss" as *; 
-          `
+    server: {
+      host: '0.0.0.0',
+      hmr: true,
+      open: false,
+      proxy: {
+        '/api': {
+          target: 'http://192.168.101.13:5000/',
+          changeOrigin: true,
+          ws: true,
+          rewrite: (path) => path.replace(new RegExp('^/api'), '')
         }
       }
-    },
-    build: buildConfig()
+    }
   }
 })
